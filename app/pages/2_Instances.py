62824228aa5b55849b82fbf98c2c1a009d84cd1c
rohitbhartiyatyson <rohit.bhartiya@tyson.com>
2025-09-26 17:19:38 +0000

    feat(instances): add instance manager and wire onboarding validation to UI

--- app/pages/2_Instances.py
+++ app/pages/2_Instances.py
@@ -16,5 +16,16 @@ selected_kind = st.selectbox('Select a Kind to onboard', options=kinds if kinds 
 
 data_file = st.file_uploader('Upload instance data file', type=['csv', 'xlsx', 'xls'])
 
+from insight_agent.instance_manager import onboard_instance
+
 if st.button('Onboard Instance'):
-    st.info('Onboard Instance functionality is not implemented yet.')
+    if selected_kind == 'No kinds available':
+        st.error('No Kind available to onboard against.')
+    elif data_file is None:
+        st.error('Please upload an instance data file.')
+    else:
+        success, message = onboard_instance(selected_kind, data_file)
+        if success:
+            st.success(message)
+        else:
+            st.error(message)
